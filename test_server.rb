a49d931bb849468d1af903a59d20b470bb2277fc
Juan C. Müller <jcmuller@gmail.com>
2012-02-03 15:52:57 -0500

    Add more urls

--- test_server.rb
+++ test_server.rb
@@ -9,6 +9,10 @@ before do
   STDERR.puts "[#{Time.now}] [test_server] #{request.path}"
 end
 
-get /\/(red|green|yellow|off)/ do |path|
+get "/" do
+  "<html>/</html>"
+end
+
+get /\/(red|green|yellow|alive|current|off)/ do |path|
   "<html>#{path}</html>"
 end
