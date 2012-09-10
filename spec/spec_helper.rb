598e1ae78004cfacc8f06252c31ad34feb9e8fe4
Juan C. Müller <jcmuller@gmail.com>
2012-09-10 12:24:13 -0400

    Load library

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@ end
 require "rspec/core"
 require "rspec/mocks"
 
+require "build_status_server"
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
