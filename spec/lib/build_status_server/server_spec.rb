990c72eac31cd9588616efb022010dbffc9b6edd
Juan C. Müller <jcmuller@gmail.com>
2012-11-19 21:33:41 -0500

    Test server

--- spec/lib/build_status_server/server_spec.rb
+++ spec/lib/build_status_server/server_spec.rb
@@ -1,4 +1,61 @@
 require 'spec_helper'
 
+class SomeClassDefinedInServer
+end
+
 describe BuildStatusServer::Server do
+  let(:config) { mock }
+  let(:store) { mock }
+
+  subject { described_class.new(config, store) }
+
+  describe "#setup" do
+    it "#noop" do
+      subject.setup
+    end
+
+    it "should raise if..." do
+      subject.stub(:class).and_return(SomeClassDefinedInServer)
+      expect{ subject.setup }.to raise_error(RuntimeError)
+    end
+  end
+
+  describe "#process" do
+    it "#noop" do
+      subject.process
+    end
+
+    it "should raise if..." do
+      subject.stub(:class).and_return(SomeClassDefinedInServer)
+      expect{ subject.process }.to raise_error(RuntimeError)
+    end
+  end
+
+  describe "#address_in_use_error" do
+    it "should show info and exit" do
+      STDERR.should_receive(:puts).with(<<-EOT)
+There appears that another instance is running, or another process
+is listening on the same port (address:port)
+
+      EOT
+      expect{ subject.address_in_use_error("address", "port") }.to raise_error SystemExit
+    end
+  end
+
+  describe "#address_not_available_error" do
+    it "should show info and exit" do
+      STDERR.should_receive(:puts).with(<<-EOT)
+The address configured is not available (address)
+
+      EOT
+      expect{ subject.address_not_available_error("address") }.to raise_error SystemExit
+    end
+  end
+
+  describe "#run_loop" do
+    it "should process" do
+      subject.should_receive(:process).and_raise(SystemExit)
+      expect{ subject.run_loop }.to raise_error(SystemExit)
+    end
+  end
 end
